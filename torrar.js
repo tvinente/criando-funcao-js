75a22493f0950cdae03a928d659acac8e72f8b72
talyt <talyta_vinente@hotmail.com>
2023-11-09 20:16:57 -0300

    exemplo de uso de função

---
+++ torrar.js
@@ -0,0 +1,11 @@
+torrar()
+
+function torrar(){
+	console.log("torrando o pão")
+    injetarPao()
+}
+
+function injetarPao(){
+	console.log("preparando para injetar o pão")
+   	 console.log("finalizado")
+}
