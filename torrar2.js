700ec93bc32b348fd429ff45a8e25e85f98a5f6f
talyt <talyta_vinente@hotmail.com>
2023-11-13 16:40:45 -0300

    inserindo função com parâmetos e edição do readme

---
+++ torrar2.js
@@ -0,0 +1,9 @@
+torrar("Pão de forma" , "Talyta" , 10.90)
+torrar("Pão na chapa" , "Taty" , 5.00 )
+
+
+function torrar(pao, nome = "Cliente" , valor = 99.99 ){
+	console.log("Torrada feita com " + pao)
+    console.log("Ela é um pedido de " + nome)
+    console.log("O valor total é: " + valor)
+}
